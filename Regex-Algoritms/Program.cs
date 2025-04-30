4fd7d500bcdd168e0cf65134be03f5eeb2872a85
NahidM8 <nahidbm@code.edu.az>
2025-04-30 17:27:46 +0400

    Add project files.

---
+++ Regex-Algoritms/Program.cs
@@ -0,0 +1,55 @@
+﻿#region Task1
+
+//int[] numbers = { 31,22,45,3,7,44,66,77};
+//Console.WriteLine("Odd numbers: ");
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] % 2 == 1)
+//    {
+//        Console.WriteLine(numbers[i]);
+//    }
+//}
+//Console.WriteLine("Even numbers: ");
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] % 2 == 0)
+//    {
+//        Console.WriteLine(numbers[i]);
+//    }
+//}
+
+#endregion
+#region Task2
+
+//int[] numbers = { 11,33,56,21,78,300,23};
+//int sum1 = 0;
+//int sum2 = 0;
+
+//for (int i = 1; i < numbers.Length; i++)
+//{
+//    if (i % 2 == 1)
+//        sum1 += numbers[i];
+//    else
+//        sum2 += numbers[i];
+//}
+
+//Console.WriteLine(sum1 - sum2);
+
+#endregion
+#region Task3
+
+//int[] numbers = { 11, 11, 11, 33, 33, 56, 56, 21, 21, 21, 21, 78, 300, 23 };
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    int count = 0;
+//    for (int j = 0; j < numbers.Length; j++)
+//    {
+//        if (numbers[j] == numbers[i])
+//            count++;
+//    }
+//    if (count == 2)
+//        Console.WriteLine(numbers[i]);
+//}
+
+#endregion
